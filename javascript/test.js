4e6c1738229f1064b16d329c28ed574517a4f8a7
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-11-15 02:14:00 +0300

    Any type problems

--- javascript/test.js
+++ javascript/test.js
@@ -21,16 +21,99 @@ console.log(add(2, 3));
 // const x: number = null;
 const x: number | null = null; // - strictNullChecks: true
  */
-class Square {
-    constructor(width) {
-        this.width = width;
-    }
+/* class Square {
+  constructor(public width: number) {}
 }
+
 class Rectangle extends Square {
-    constructor(width, height) {
-        super(width);
-        this.width = width;
-        this.height = height;
-    }
+  constructor(public width: number, public height: number) {
+    super(width);
+  }
 }
+
 let square = new Rectangle(10, 10);
+ */
+/*
+// - Structural typing
+
+interface Vector2D {
+  x: number;
+  y: number;
+}
+
+function calculateLength(v: Vector2D) {
+  return Math.sqrt(v.x * v.x + v.y * v.y);
+}
+
+interface NamedVector {
+  name: string;
+  x: number;
+  y: number;
+}
+
+const v: NamedVector = { x: 3, y: 4, name: 'Zee' };
+
+console.log(calculateLength(v));
+
+interface Vector3D {
+  x: number;
+  y: number;
+  z: number;
+}
+// Types are alwasys open in TypeScript
+function normalize(v: Vector3D) {
+  let length = calculateLength(v);
+  return {
+    x: v.x / length,
+    y: v.y / length,
+    z: v.z / length
+  };
+}
+
+console.log(normalize({ x: 3, y: 4, z: 5 }));
+
+// Since 'v' can have more properties than Vector3D,
+// we can't guarantee that 'v' has 3 properties: x, y, z
+function calculateLength1(v: Vector3D) {
+  let length = 0;
+  for (const axis of Object.keys(v)) {
+    const coord = v[axis];
+    length += Math.abs(coord);
+  }
+  return length;
+}
+
+// Alternative implemtation
+function calculatteLength2(v: Vector3D) {
+  return Math.abs(v.x) + Math.abs(v.y) + Math.abs(v.z);
+}
+
+class C {
+  foo: String;
+  constructor(foo: string) {
+    this.foo = foo;
+  }
+}
+
+const c = new C('instance of C');
+const d: C = { foo: 'object literal' };
+ */
+// - Any type pasks bugs and stops Typescript language
+// - service from helping you. (no autocomplete for type any),
+// - (no rename for type any)
+// it lets you do things that are not type safe
+let age;
+age = '12';
+age = '12';
+age += 1;
+console.log(age);
+// it breaks contracts
+function calcuateAge(birthDate) { return 0; }
+let birthDate = '1990-01-01';
+calcuateAge(birthDate);
+function renderSelector(props) { }
+let selectedId = 0;
+function handleSelectedItem(item) {
+    selectedId = item.id;
+}
+renderSelector({ onSelectItem: handleSelectedItem });
--- test.ts
+++ test.ts
@@ -23,7 +23,7 @@ const x: number | null = null; // - strictNullChecks: true
  */
 
 
-class Square {
+/* class Square {
   constructor(public width: number) {}
 }
 
@@ -34,3 +34,102 @@ class Rectangle extends Square {
 }
 
 let square = new Rectangle(10, 10);
+ */
+
+/*
+// - Structural typing
+
+interface Vector2D {
+  x: number;
+  y: number;
+}
+
+function calculateLength(v: Vector2D) {
+  return Math.sqrt(v.x * v.x + v.y * v.y);
+}
+
+interface NamedVector {
+  name: string;
+  x: number;
+  y: number;
+}
+
+const v: NamedVector = { x: 3, y: 4, name: 'Zee' };
+
+console.log(calculateLength(v));
+
+interface Vector3D {
+  x: number;
+  y: number;
+  z: number;
+}
+// Types are alwasys open in TypeScript
+function normalize(v: Vector3D) {
+  let length = calculateLength(v);
+  return {
+    x: v.x / length,
+    y: v.y / length,
+    z: v.z / length
+  };
+}
+
+console.log(normalize({ x: 3, y: 4, z: 5 }));
+
+// Since 'v' can have more properties than Vector3D,
+// we can't guarantee that 'v' has 3 properties: x, y, z
+function calculateLength1(v: Vector3D) {
+  let length = 0;
+  for (const axis of Object.keys(v)) {
+    const coord = v[axis];
+    length += Math.abs(coord);
+  }
+  return length;
+}
+
+// Alternative implemtation
+function calculatteLength2(v: Vector3D) {
+  return Math.abs(v.x) + Math.abs(v.y) + Math.abs(v.z);
+}
+
+class C {
+  foo: String;
+  constructor(foo: string) {
+    this.foo = foo;
+  }
+}
+
+const c = new C('instance of C');
+const d: C = { foo: 'object literal' };
+ */
+
+// - Any type pasks bugs and stops Typescript language
+// - service from helping you. (no autocomplete for type any),
+// - (no rename for type any)
+
+// it lets you do things that are not type safe
+let age: number;
+age = '12';
+age = '12' as any;
+age += 1;
+console.log(age);
+
+// it breaks contracts
+function calcuateAge(birthDate: Date): number { return 0; }
+let birthDate: any = '1990-01-01'
+calcuateAge(birthDate);
+
+// version that masks bugs (using any) of ComponentProps
+// interface ComponentProps {
+//   onSelectItem: (item: any) => void;
+// }
+
+// reworked version of ComponentProps
+interface ComponentProps {
+  onSelectItem: (item: number) => void;
+}
+function renderSelector(props: ComponentProps) { /* ... */ }
+let selectedId: number = 0;
+function handleSelectedItem(item: any) {
+  selectedId = item.id;
+}
+renderSelector({ onSelectItem: handleSelectedItem});
